a7dea72bd45419d114f9e5dada4155737651c378
ibkkkkkk <156062843+ibkkkkkk@users.noreply.github.com>
2025-07-23 11:00:23 +0100

    add platform hook

--- .platform/hooks/prebuild/install.sh
+++ .platform/hooks/prebuild/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Installing node modules with legacy-peer-deps"
+echo "Installing node modules with legacy-peer-deps for elasticbeanstalk"
 npm install --omit=dev --legacy-peer-deps
